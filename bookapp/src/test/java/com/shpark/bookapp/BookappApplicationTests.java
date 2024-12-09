5b7214558fc99fb9ac61edcd2511cff219503a1d
touchme1123 <touchme11309@gmail.com>
2024-12-10 08:57:26 +0900

    project start

---
+++ bookapp/src/test/java/com/shpark/bookapp/BookappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shpark.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
