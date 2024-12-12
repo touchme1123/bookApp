c5589ee6916f08810860e8c1332733216ef4a582
touchme1123 <touchme11309@gmail.com>
2024-12-12 13:17:20 +0900

    [Feature] 이미지/썸네일 업로드처리 기능

--- bookapp/src/main/java/com/shpark/bookapp/controller/BookController.java
+++ bookapp/src/main/java/com/shpark/bookapp/controller/BookController.java
@@ -4,10 +4,12 @@ import com.shpark.bookapp.dto.BookDTO;
 import com.shpark.bookapp.util.CustomFileUitl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,9 +20,20 @@ public class BookController {
 
     private final CustomFileUitl fileUitl;
 
-    @PostMapping("/register")
+    @PostMapping("/")
     public Map<String,String> register(BookDTO bookDTO) {
         log.info("-----register-----");
-        return null;
+
+        List<MultipartFile> files = bookDTO.getFiles();
+        List<String> uploadedFileNames = fileUitl.saveFiles(files);
+
+        bookDTO.setUploadedFileNames(uploadedFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGet(@PathVariable("fileName") String fileName) {
+        return fileUitl.getFile(fileName);
     }
 }
--- bookapp/src/main/java/com/shpark/bookapp/util/CustomFileUitl.java
+++ bookapp/src/main/java/com/shpark/bookapp/util/CustomFileUitl.java
@@ -3,11 +3,17 @@ package com.shpark.bookapp.util;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -38,27 +44,70 @@ public class CustomFileUitl {
     }
 
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
-        if(files == null || files.size() == 0) {
+        if (files == null || files.size() == 0) {
             return List.of();
         }
 
         List<String> uploadNames = new ArrayList<>();
 
-        for(MultipartFile file : files) {
+        for (MultipartFile file : files) {
             String savedName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
             Path savePath = Paths.get(uploadPath, savedName);
 
             try {
                 Files.copy(file.getInputStream(), savePath);
+
+                //썸네일 처리
+                String contentType = file.getContentType(); // Mime
+                if (contentType.equals("image/jpeg")) {
+
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+
+                    Thumbnails.of(savePath.toFile()).size(200, 200).toFile(thumbnailPath.toFile());
+                }
+
                 uploadNames.add(savedName);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
+        return uploadNames;
+    }
 
+    public ResponseEntity<Resource> getFile(String fileName) {
 
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
 
+        if (!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
+        }
 
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    public void deleteFiles(List<String> fileNames) {
+        if (fileNames == null || fileNames.size() == 0) {
+            return;
+        }
+        fileNames.forEach(fileName -> {
+            String thumbnailFileNames = "s_" + fileName;
+
+            Path thumbnailPath = Paths.get(uploadPath, thumbnailFileNames);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try {
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnailPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
